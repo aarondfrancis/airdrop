d53bdb03f725c75a3b111cbce9bb9df52e5a725d
Aaron Francis <aarondfrancis@gmail.com>
2021-01-22 11:55:06 -0600

    Simplify test

--- tests/HashCalculationTest.php
+++ tests/HashCalculationTest.php
@@ -24,12 +24,16 @@ class HashCalculationTest extends TestCase
     {
         $filesTrigger = InputFilesTrigger::class;
 
-        config()->set("airdrop.triggers.$filesTrigger.include", [
-            __DIR__ . '/Support/primary-webpack.mix.example',
+        config()->set('airdrop.triggers', [
+            $filesTrigger => [
+                'include' => [
+                    __DIR__ . '/Support/primary-webpack.mix.example',
+                ]
+            ]
         ]);
 
-        $hash = (new HashGenerator(config('airdrop')))->generate();
+        $hash = (new HashGenerator)->generate();
 
-        $this->assertEquals('0abf20361a1fcbd930f0f2a1b80ba9b9', $hash);
+        $this->assertEquals('61e6e46a0b7bb21ba9225b9ddf7b7255', $hash);
     }
 }
